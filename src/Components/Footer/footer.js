3c8c935cd60d289843aa19f2e451ba8a2351b833
ivanatomic98 <ivanat787@gmail.com>
2022-07-26 09:01:09 +0200

    first commit

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import footer from '../Footer/footer.css'
 
 const Footer = () => {
     return (
         <footer className="footer">
-           
+           <h2>This is footer</h2>
         </footer>
     )
 }
